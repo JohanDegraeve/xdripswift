7549a9ed57a2e9a30acea1ee1ddd2db28a65d668
Johan Degraeve <johan.degraeve@gmail.com>
2019-05-31 15:20:40 +0200

    Fix for G5

--- xdrip/Transmitter/CGMBluetoothTransmitter/G5/G5Messages/SensorDataRxMessage.swift
+++ xdrip/Transmitter/CGMBluetoothTransmitter/G5/G5Messages/SensorDataRxMessage.swift
@@ -17,9 +17,7 @@ struct SensorDataRxMessage: TransmitterRxMessage {
         }
 
         status = data[1]
-        let timestampAsFixedWidthInteger:UInt32 = data[2..<6].toInt()
-        let timeStampAsDouble = Double(timestampAsFixedWidthInteger)
-        timestamp = Date(timeIntervalSince1970: timeStampAsDouble)
+        timestamp = Date()
         let filteredAsUint32:UInt32 = data[6..<10].toInt()
         filtered = Double(filteredAsUint32)
         let unfilteredAsUint32:UInt32 = data[10..<14].toInt()
