0ad8dd8bf70fb9b1f9b9fc53a35e2b535c335298
huyan_18040019 <huyan@shinhofood.com>
2019-09-09 16:27:03 +0800

    set alert time 5 minutes

--- xdrip/Constants/ConstantsMaster.swift
+++ xdrip/Constants/ConstantsMaster.swift
@@ -2,5 +2,5 @@
 enum ConstantsMaster {
     
     /// maximum age in seconds, of reading in alert flow. If age of latest reading is more than this number, then no alert check will be done
-    static let maximumBgReadingAgeForAlertsInSeconds = 60.0
+    static let maximumBgReadingAgeForAlertsInSeconds = 60.0 * 5
 }
