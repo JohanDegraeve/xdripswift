6feb607f41435770b415d573dba8398a603b04b7
Johan Degraeve <johan.degraeve@gmail.com>
2019-09-22 15:52:37 +0200

    add M5StackName entity

---
+++ xdrip/Core Data/classes/M5StackName+CoreDataProperties.swift
@@ -0,0 +1,22 @@
+//
+//  M5StackName+CoreDataProperties.swift
+//  
+//
+//  Created by Johan Degraeve on 22/09/2019.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension M5StackName {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<M5StackName> {
+        return NSFetchRequest<M5StackName>(entityName: "M5StackName")
+    }
+
+    @NSManaged public var address: String?
+    @NSManaged public var userDefinedName: String?
+
+}
