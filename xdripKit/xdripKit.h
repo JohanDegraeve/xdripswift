c882a12f539e1ebf0cadd6cfe6c18d92032c3179
Cloves Jr <clvsjr9@gmail.com>
2020-02-07 15:22:10 -0300

    xdripKit target creation to be used with future xdripClient of Loop

---
+++ xdripKit/xdripKit.h
@@ -0,0 +1,19 @@
+//
+//  xdripKit.h
+//  xdripKit
+//
+//  Created by Cloves Ferreira Jr on 07/02/20.
+//  Copyright © 2020 Johan Degraeve. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for xdripKit.
+FOUNDATION_EXPORT double xdripKitVersionNumber;
+
+//! Project version string for xdripKit.
+FOUNDATION_EXPORT const unsigned char xdripKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <xdripKit/PublicHeader.h>
+
+
