84d20d2912a16101db598247a04ff2eb9ddb3a41
Ivan Valkou <zloyfey@gmail.com>
2020-07-27 11:58:57 +0300

    Removed extra new lines

--- xdrip/View Controllers/SettingsNavigationController/SettingsViewController/SettingsViewModels/SettingsViewLibreToolsViewModel.swift
+++ xdrip/View Controllers/SettingsNavigationController/SettingsViewController/SettingsViewModels/SettingsViewLibreToolsViewModel.swift
@@ -14,65 +14,47 @@ fileprivate enum Setting:Int, CaseIterable {
 }
 
 struct SettingsViewLibreToolsViewModel: SettingsViewModelProtocol {
-
     func sectionTitle() -> String? { "Freestyle Libre" }
 
     func settingsRowText(index: Int) -> String {
-
         guard let setting = Setting(rawValue: index) else { fatalError("Unexpected Section") }
 
         switch setting {
         case .libreTools:
             return "Libre tools"
         }
-
     }
 
     func accessoryType(index: Int) -> UITableViewCell.AccessoryType {
-
         guard let setting = Setting(rawValue: index) else { fatalError("Unexpected Section") }
 
         switch setting {
-
         case .libreTools:
             return .disclosureIndicator
-
         }
-
     }
 
     func detailedText(index: Int) -> String? {
-
         guard let setting = Setting(rawValue: index) else { fatalError("Unexpected Section") }
 
         switch setting {
-
         case .libreTools:
-
             return nil
-
         }
     }
 
-    func uiView(index: Int) -> UIView? {
-        return nil
-    }
+    func uiView(index: Int) -> UIView? { nil }
 
-    func numberOfRows() -> Int {
-        return Setting.allCases.count
-    }
+    func numberOfRows() -> Int { Setting.allCases.count }
 
     func onRowSelect(index: Int) -> SettingsSelectedRowAction {
-
         guard let setting = Setting(rawValue: index) else { fatalError("Unexpected Section") }
 
         switch setting {
-
         case .libreTools:
            if #available(iOS 13, *) {
                 return .presentModal(viewController: LibreTools.makeViewController())
             }
-
             return .nothing
         }
     }
