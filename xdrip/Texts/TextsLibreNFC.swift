9d0c2a19329e6e6cde294421b6cf1b3fc7df8458
Paul Plant <37302780+paulplant@users.noreply.github.com>
2021-01-16 20:11:23 +0100

    Libre2 NFC Scan Texts eng + esp

--- xdrip/Texts/TextsLibreNFC.swift
+++ xdrip/Texts/TextsLibreNFC.swift
@@ -9,19 +9,19 @@ class TextsLibreNFC {
     }()
 
     static let holdTopOfIphoneNearSensor: String = {
-        return NSLocalizedString("holdTopOfIphoneNearSensor", tableName: filename, bundle: Bundle.main, value: "Hold the top of your iOS device near the sensor", comment: "when NFC scanning is started, this message will appear")
+        return NSLocalizedString("holdTopOfIphoneNearSensor", tableName: filename, bundle: Bundle.main, value: "Hold the top of your iOS device near the sensor to scan", comment: "when NFC scanning is started, this message will appear")
     }()
     
     static let deviceMustSupportNFC: String = {
-        return NSLocalizedString("deviceMustSupportNFC", tableName: filename, bundle: Bundle.main, value: "Device must support NFC", comment: "Device must support NFC")
+        return NSLocalizedString("deviceMustSupportNFC", tableName: filename, bundle: Bundle.main, value: "This iPhone does not support NFC", comment: "Device must support NFC")
     }()
     
     static let deviceMustSupportIOS14: String = {
-        return NSLocalizedString("deviceMustSupportIOS14", tableName: filename, bundle: Bundle.main, value: "Device must support at least iOS 14.0", comment: "Device must support at least iOS 14.0")
+        return NSLocalizedString("deviceMustSupportIOS14", tableName: filename, bundle: Bundle.main, value: "To connect to Libre 2, this iPhone needs upgrading to iOS14", comment: "Device must support at least iOS 14.0")
     }()
     
     static let donotusethelibrelinkapp: String = {
-        return NSLocalizedString("donotusethelibrelinkapp", tableName: filename, bundle: Bundle.main, value: "Connected to Libre 2.\r\n\r\nIf you still want to scan the Libre sensor with the official Libre app, then disallow bluetooth permission for the Libre app. Otherwise, scanning the NFC with the Libre app and with bluetooth permission allowed will disallow xDrip4iOS to connect to the Libre 2", comment: "After Libre NFC scanning, and after successful bluetooth connection, this message will be shown to explain that he or she should not allow bluetooth permission on the Libre app")
+        return NSLocalizedString("donotusethelibrelinkapp", tableName: filename, bundle: Bundle.main, value: "Connected to Libre 2.\r\n\r\nIf you want to keeping scanning sometimes the Libre sensor with the official Libre app, then disallow bluetooth permission for the Libre app. \r\n\r\nOtherwise, scanning the NFC with the Libre app and with the bluetooth permission allowed will break the connection between xDrip4iOS and the Libre 2.", comment: "After Libre NFC scanning, and after successful bluetooth connection, this message will be shown to explain that he or she should not allow bluetooth permission on the Libre app")
     }()
     
 }
