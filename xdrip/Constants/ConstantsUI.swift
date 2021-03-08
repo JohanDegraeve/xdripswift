48ca2795b41292d4dc94765848ea804660dfd0bf
Johan Degraeve <johan.degraeve@gmail.com>
2021-03-08 21:13:55 +0100

    when selecting a cell in tableview : set background color to blue

--- xdrip/Constants/ConstantsUI.swift
+++ xdrip/Constants/ConstantsUI.swift
@@ -6,4 +6,7 @@ enum ConstantsUI {
     /// color for section titles in grouped table views, example in settings view
     static let tableViewHeaderTextColor = UIColor.systemBlue
     
+    /// color to use as background when a row is selected
+    static let tableRowSelectedBackGroundColor = UIColor.systemBlue
+    
 }
