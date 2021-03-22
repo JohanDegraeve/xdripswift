257261616226391a869de0dbc09e3186bcbe14d3
Paul Plant <paulplant@gmail.com>
2021-03-22 22:27:23 +0100

    Menu colour updates

--- xdrip/Constants/ConstantsUI.swift
+++ xdrip/Constants/ConstantsUI.swift
@@ -4,12 +4,12 @@ import UIKit
 enum ConstantsUI {
     
     /// color for section titles in grouped table views, example in settings view
-    static let tableViewHeaderTextColor = UIColor.systemBlue
+    static let tableViewHeaderTextColor = UIColor.lightGray
     
     /// color to use as background when a row is selected
-    static let tableRowSelectedBackGroundColor = UIColor.systemBlue
+    static let tableRowSelectedBackGroundColor = UIColor.darkGray
     
     /// color to use for disclosure indicator in settings views
-    static let disclosureIndicatorColor = UIColor.systemBlue
+    static let disclosureIndicatorColor = UIColor.gray
     
 }
