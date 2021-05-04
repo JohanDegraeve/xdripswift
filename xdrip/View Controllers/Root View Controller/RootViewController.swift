cfc2d4a0daff31ed4ee7b8b76fd97bce4eebcf62
Paul Plant <37302780+paulplant@users.noreply.github.com>
2021-05-04 22:37:58 +0200

    ensure statistics are calculated at app start up
    Co-Authored-By: Johan Degraeve <13840461+JohanDegraeve@users.noreply.github.com>
    (cherry picked from commit 30e90e02b5b8fbb0a3109ad29c45ba1e1077d8cf)

--- xdrip/View Controllers/Root View Controller/RootViewController.swift
+++ xdrip/View Controllers/Root View Controller/RootViewController.swift
@@ -309,7 +309,7 @@ final class RootViewController: UIViewController {
         spacerView.isHidden = UserDefaults.standard.showStatistics
         
         // update statistics related outlets
-        updateStatistics(animatePieChart: true)
+        updateStatistics(animatePieChart: true, overrideApplicationState: true)
         
 
         
@@ -365,7 +365,7 @@ final class RootViewController: UIViewController {
             self.updateLabelsAndChart(overrideApplicationState: true)
             
             // update statistics related outlets
-            self.updateStatistics(animatePieChart: true)
+            self.updateStatistics(animatePieChart: true, overrideApplicationState: true)
             
             // create badge counter
             self.createBgReadingNotificationAndSetAppBadge(overrideShowReadingInNotification: true)
@@ -1746,10 +1746,10 @@ final class RootViewController: UIViewController {
     
     
     // helper function to calculate the statistics and update the pie chart and label outlets
-    private func updateStatistics(animatePieChart: Bool = false) {
+    private func updateStatistics(animatePieChart: Bool = false, overrideApplicationState: Bool = false) {
         
         // don't calculate statis if app is not running in the foreground
-        guard UIApplication.shared.applicationState == .active else {return}
+        guard UIApplication.shared.applicationState == .active || overrideApplicationState else {return}
         
         // if the user doesn't want to see the statistics, then just return without doing anything
         if !UserDefaults.standard.showStatistics {
@@ -1761,6 +1761,7 @@ final class RootViewController: UIViewController {
         var daysToUseStatistics: Int = 0
         var fromDate: Date = Date()
         
+        debuglogging("in updateStatistics")
         
         // get the maximum number of calculation days requested by the user
         daysToUseStatistics = UserDefaults.standard.daysToUseStatistics
