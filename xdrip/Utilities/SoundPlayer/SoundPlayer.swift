8685a88b8b180bddbbc44909710f39ab3ee675a6
Samuli Tamminen <samuli.tamminen@gmail.com>
2022-10-07 19:47:36 +0300

    change SoundPlayer to handle queue of sound files

--- xdrip/Utilities/SoundPlayer/SoundPlayer.swift
+++ xdrip/Utilities/SoundPlayer/SoundPlayer.swift
@@ -5,7 +5,7 @@ import os
 import Speech
 
 /// to play audio and speak text, overrides mute
-class SoundPlayer {
+class SoundPlayer: NSObject {
     
     // MARK: - properties
     
@@ -14,6 +14,9 @@ class SoundPlayer {
 
     /// audioplayer
     private var audioPlayer:AVAudioPlayer?
+
+    /// list of audio files to play after each other
+    private var playingQueue = [String]()
     
     // MARK: - initializer
     
@@ -21,9 +24,35 @@ class SoundPlayer {
     /// - parameters:
     ///     - soundFileName : name of the file with the sound, the filename must include the extension, eg mp3
     public func playSound(soundFileName:String) {
+        playSound(soundFileNames: [soundFileName])
+    }
+
+    /// plays the sounds sequentially, overrides mute
+    /// - parameters:
+    ///     - soundFileNames : names of the files with the sound, the filenames must include the extension, eg mp3
+    public func playSound(soundFileNames: [String]) {
+        // Add the requested files to be played
+        playingQueue += soundFileNames
+
+        // If the player is not already playing, play the first file
+        if !isPlaying() {
+            dequeueFileIfAny()
+        }
+    }
+
+    /// Play the first file in the queue, if any
+    private func dequeueFileIfAny() {
+        // Exit early if there's nothing to do.
+        // This is expected as we call this in `audioPlayerDidFinishPlaying` to play the next sound in queue
+        if playingQueue.isEmpty {
+            return
+        }
+
+        // First in, first out of the queue
+        let soundFileName = playingQueue.removeFirst()
         
         guard let url = Bundle.main.url(forResource: soundFileName, withExtension: "") else {
-            trace("in playSound, could not create url with sound %{public}@", log: self.log, category: ConstantsLog.categoryPlaySound, type: .error, soundFileName)
+            trace("in SoundPlayer, could not create url with sound %{public}@", log: self.log, category: ConstantsLog.categoryPlaySound, type: .error, soundFileName)
             return
         }
         
@@ -36,6 +65,9 @@ class SoundPlayer {
         
         do {
             try audioPlayer = AVAudioPlayer(contentsOf: url)
+
+            // Set delegate to get callback when the audio finished playing
+            audioPlayer?.delegate = self
             
             if let audioPlayer = audioPlayer {
                 audioPlayer.play()
@@ -63,5 +95,13 @@ class SoundPlayer {
             }
         }
     }
-    
+}
+
+// MARK: - AVAudioPlayerDelegate
+
+extension SoundPlayer: AVAudioPlayerDelegate {
+    func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
+        // Play next in the queue. This enables adding a bunch of files to be played after each other.
+        dequeueFileIfAny()
+    }
 }
