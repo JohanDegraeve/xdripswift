da353e85cd1a5729b63f9c11857d4c55ea6580b9
Paul Plant <37302780+paulplant@users.noreply.github.com>
2023-01-09 20:09:42 +0100

    Calendar event visual indicator (#411)
    * Initial adding "Visual indicator" setting for calendar title
    
    * removed shape bits
    
    Redesigning the idea, so the each shape can be defined by the user
    
    * Refactored, concept of visual indicator into a combination of extension and constant files
    
    * revert file
    
    * Update MainInterface.storyboard
    
    * Tidying up comments in the watchManager
    
    * fixed typo in enum property inRange
    
    * renamed the settings variable to copy the existing settings and changed settings order
    
    * Fixed missed properties to align the names with the settings
    
    Co-authored-by: gonkowonko <4158482+gonkowonko@users.noreply.github.com>

---
+++ xdrip/Constants/ConstantsBgRange.swift
@@ -0,0 +1,20 @@
+//
+//  ConstantsBgRange.swift
+//  xdrip
+//
+//  Created by Gary Dalley on 12/11/2022.
+//  Copyright © 2022 Johan Degraeve. All rights reserved.
+//
+
+/// Constants for blood glucose ranges
+enum BgRange {
+    
+    /// bg range is "urgent"
+    case urgent
+    
+    /// bg range is "not urgent"
+    case notUrgent
+    
+    /// bg range is "in range"
+    case inRange
+}
--- xdrip/Constants/ConstantsWatch.swift
+++ xdrip/Constants/ConstantsWatch.swift
@@ -5,4 +5,12 @@ enum ConstantsWatch {
     /// text to add as notes in glucose events
     static let textInCreatedEvent = "created by xdrip"
     
+    /// text to use as the visual indicator in the calendar title when bg is "Urgent"
+    static let visualIndicatorUrgent = "🔴"
+    
+    /// text to use as the visual indicator in the calendar title when bg is "Not Urgent"
+    static let visualIndicatorNotUrgent = "🟡"
+    
+    /// text to use as the visual indicator in the calendar title when bg is "In Range"
+    static let visualIndicatorInRange = "🟢"
 }
