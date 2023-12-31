9944f1f5d10e1e8af8fc25f5accd1a9464926814
Guy Shaviv <guy@shaviv.org>
2023-12-31 10:31:59 +0200

    play with response graph scale

--- xdrip/GlucoseIntentResponseView.swift
+++ xdrip/GlucoseIntentResponseView.swift
@@ -29,7 +29,36 @@ struct GlucoseIntentResponseView: View {
     }
 
     var body: some View {
+        let domain = min(readings.map(\.valueInUserUnits).min() ?? Double.greatestFiniteMagnitude, 70 * unitFactor) ... max(readings.map(\.valueInUserUnits).max() ?? -Double.greatestFiniteMagnitude, 180 * unitFactor)
         Chart {
+            if domain.contains(UserDefaults.standard.urgentLowMarkValueInUserChosenUnit) {
+                RuleMark(y: .value("", UserDefaults.standard.urgentLowMarkValueInUserChosenUnit))
+                    .lineStyle(StrokeStyle(lineWidth: 1, dash: [1, 3]))
+                    .foregroundStyle(.red)
+            }
+            if domain.contains(UserDefaults.standard.urgentLowMarkValueInUserChosenUnit) {
+                RuleMark(y: .value("", UserDefaults.standard.urgentLowMarkValueInUserChosenUnit))
+                    .lineStyle(StrokeStyle(lineWidth: 1, dash: [1, 3]))
+                    .foregroundStyle(.red)
+            }
+
+            if domain.contains(UserDefaults.standard.lowMarkValueInUserChosenUnit) {
+                RuleMark(y: .value("", UserDefaults.standard.lowMarkValueInUserChosenUnit))
+                    .lineStyle(StrokeStyle(lineWidth: 2, dash: [2, 4]))
+                    .foregroundStyle(.yellow)
+            }
+            if domain.contains(UserDefaults.standard.highMarkValueInUserChosenUnit) {
+                RuleMark(y: .value("", UserDefaults.standard.highMarkValueInUserChosenUnit))
+                    .lineStyle(StrokeStyle(lineWidth: 2, dash: [2, 4]))
+                    .foregroundStyle(.yellow)
+            }
+
+            if domain.contains(UserDefaults.standard.targetMarkValueInUserChosenUnit) {
+                RuleMark(y: .value("", UserDefaults.standard.targetMarkValueInUserChosenUnit))
+                    .lineStyle(StrokeStyle(lineWidth: 2, dash: [8, 8]))
+                    .foregroundStyle(.green)
+            }
+
             ForEach(readings, id: \.timeStamp) { reading in
                 PointMark(x: .value("Time", reading.timeStamp),
                           y: .value("BG", reading.valueInUserUnits))
@@ -38,7 +67,7 @@ struct GlucoseIntentResponseView: View {
             }
         }
         .chartXAxis {
-            AxisMarks() {
+            AxisMarks {
                 if let v = $0.as(Date.self) {
                     AxisValueLabel {
                         Text(v.formatted(.dateTime.hour()))
@@ -50,20 +79,23 @@ struct GlucoseIntentResponseView: View {
             }
         }
         .chartYAxis {
-            AxisMarks(values: [UserDefaults.standard.urgentLowMarkValueInUserChosenUnit,
-                               UserDefaults.standard.lowMarkValueInUserChosenUnit,
-                               UserDefaults.standard.targetMarkValueInUserChosenUnit,
-                               UserDefaults.standard.highMarkValueInUserChosenUnit,
-                               UserDefaults.standard.urgentHighMarkValueInUserChosenUnit]) {
-                AxisValueLabel()
-                    .foregroundStyle(Color.white)
-                AxisGridLine()
-                    .foregroundStyle(Color.white)
+            AxisMarks(values: axisTicks(for: domain)) { value in
+                if let v = value.as(Double.self) {
+                    AxisValueLabel {
+                        Text(v.formatted(.number.precision(.significantDigits(UserDefaults.standard.bloodGlucoseUnitIsMgDl ? 0 : 1))))
+                            .font(.caption2)
+                    }
+
+                    if !userValues.contains(v) {
+                        AxisValueLabel()
+                            .foregroundStyle(Color.white)
+                        AxisGridLine()
+                            .foregroundStyle(Color.white)
+                    }
+                }
             }
         }
-        .chartYScale(domain:
-            min(readings.map(\.valueInUserUnits).min() ?? Double.greatestFiniteMagnitude, UserDefaults.standard.urgentLowMarkValueInUserChosenUnit) ... max(readings.map(\.valueInUserUnits).max() ?? -Double.greatestFiniteMagnitude, UserDefaults.standard.urgentHighMarkValueInUserChosenUnit)
-        )
+        .chartYScale(domain: domain)
         .aspectRatio(1.5, contentMode: .fit)
         .padding()
         .background {
@@ -74,6 +106,32 @@ struct GlucoseIntentResponseView: View {
 
 extension BgReading {
     var valueInUserUnits: Double {
-        UserDefaults.standard.bloodGlucoseUnitIsMgDl ? calculatedValue : calculatedValue * ConstantsBloodGlucose.mgDlToMmoll
+        calculatedValue * unitFactor
+    }
+}
+
+private var unitFactor: Double {
+    UserDefaults.standard.bloodGlucoseUnitIsMgDl ? 1 : ConstantsBloodGlucose.mgDlToMmoll
+}
+
+private var userValues = [UserDefaults.standard.urgentLowMarkValueInUserChosenUnit,
+                          UserDefaults.standard.lowMarkValueInUserChosenUnit,
+                          UserDefaults.standard.targetMarkValueInUserChosenUnit,
+                          UserDefaults.standard.highMarkValueInUserChosenUnit,
+                          UserDefaults.standard.urgentHighMarkValueInUserChosenUnit]
+
+private func axisTicks(for domain: ClosedRange<Double>) -> [Double] {
+    var values = [70, 180, 250].map { $0 * unitFactor }.filter { domain.contains($0) }
+    for v in userValues where domain.contains(v) {
+        if values.filter({ abs(v - $0) < 9.9 }).isEmpty {
+            values.append(v)
+        }
+    }
+    return values
+}
+
+extension [Double] {
+    var range: ClosedRange<Double> {
+        (self.min() ?? -Double.greatestFiniteMagnitude) ... (self.max() ?? Double.greatestFiniteMagnitude)
     }
 }
