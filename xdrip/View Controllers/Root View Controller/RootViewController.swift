0397dab23ad508ae3ce2f9ad9a1e2507bc74f547
Samuli Tamminen <samuli.tamminen@gmail.com>
2024-03-30 08:32:06 +0200

    lower the number of readings to send to live activity to stay under 4kB

--- xdrip/View Controllers/Root View Controller/RootViewController.swift
+++ xdrip/View Controllers/Root View Controller/RootViewController.swift
@@ -3502,7 +3502,7 @@ final class RootViewController: UIViewController, ObservableObject {
 
                     // Live Activities have maximum payload size of 4kB.
                     // This value is selected by testing how much we can send before getting the "Payload maximum size exceeded" error.
-                    let maxNumberOfReadings = 275
+                    let maxNumberOfReadings = 260
 
                     // If there are more readings than we can send to the Live Activity, downsample the values to fit.
                     let bgReadings = allBgReadings.count > maxNumberOfReadings
