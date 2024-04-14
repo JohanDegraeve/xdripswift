8a1be8928a38189b35eec54dd4255a1db712df3f
Paul Plant <37302780+paulplant@users.noreply.github.com>
2024-04-14 14:35:42 +0200

    forceComplicationRefreshTimeInMinutes to 20 minutes
    - change back to 20 minute forced refresh intervals
    
    - using 10 minutes (correctly) runs out of remainingComplicationUserInfoTransfers by around 17hrs

--- xdrip/Constants/ConstantsWidget.swift
+++ xdrip/Constants/ConstantsWidget.swift
@@ -13,5 +13,5 @@ enum ConstantsWidget {
     /// the amount of time that should pass before a watch complication refresh is forced
     /// bear in mind there is a limit to 50 times per day whilst the Watch session is available
     /// the theoretical calculation could be 17 hours / 50 times = every 20.4 minutes but this needs testing on a real device
-    static let forceComplicationRefreshTimeInMinutes = TimeInterval(minutes: 10)
+    static let forceComplicationRefreshTimeInMinutes = TimeInterval(minutes: 20)
 }
