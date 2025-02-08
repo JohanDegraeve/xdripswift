2f3e349f0e15ede294b558b654232cb2e279c906
Paul Plant <37302780+paulplant@users.noreply.github.com>
2025-02-08 18:50:16 +0100

    Update Trace.swift
    Co-Authored-By: dugamarian <35396265+dugamarian@users.noreply.github.com>

--- xdrip/Utilities/Trace.swift
+++ xdrip/Utilities/Trace.swift
@@ -572,7 +572,7 @@ class Trace {
                             
                             traceInfo.appendStringAndNewLine("        Read from Dexcom app: " + dexcomG5.useOtherApp.description)
                             
-                            traceInfo.appendStringAndNewLine("        Is Anubis? " + dexcomG5.isAnubis.description)
+                            traceInfo.appendStringAndNewLine("        Is Anubis?: " + dexcomG5.isAnubis.description)
                             
                             traceInfo.appendStringAndNewLine("        Last reset: " + (dexcomG5.lastResetTimeStamp?.toString(timeStyle: .short, dateStyle: .medium) ?? "nil") + " (" + (dexcomG5.lastResetTimeStamp?.daysAndHoursAgo(appendAgo: true) ?? "nil") + ")")
                             
