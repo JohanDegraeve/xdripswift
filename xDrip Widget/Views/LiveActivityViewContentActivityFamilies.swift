78433a413af3d42eefcfa621f901e1ef0dd61372
Paul Plant <37302780+paulplant@users.noreply.github.com>
2026-06-14 20:44:28 +0200

    Darken watch Smart Stack Live Activity background and fix opaque banner

--- xDrip Widget/Views/LiveActivityViewContentActivityFamilies.swift
+++ xDrip Widget/Views/LiveActivityViewContentActivityFamilies.swift
@@ -18,6 +18,12 @@ struct LiveActivityViewContentActivityFamilies: View {
     
     var body: some View {
         ZStack {
+            AccessoryWidgetBackground()
+                .cornerRadius(8)
+            
+            RoundedRectangle(cornerRadius: 8)
+                .fill(Color.black.opacity(0.4))
+            
             GeometryReader { geo in
                 VStack(spacing: 3) {
                     HStack(alignment: .center) {
@@ -63,7 +69,7 @@ struct LiveActivityViewContentActivityFamilies: View {
                 }
             }
         }
-        
+        .activityBackgroundTint(.clear)
     }
 }
 
