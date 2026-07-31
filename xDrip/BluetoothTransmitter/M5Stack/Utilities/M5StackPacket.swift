7a5a724cacc3290269cb8873b8be042e52c1f4f8
Paul Plant <37302780+paulplant@users.noreply.github.com>
2026-07-31 23:47:13 +0200

    Validate M5Stack packet numbering
    Reject zero and out-of-range packet headers before indexing the receive buffer.
    
    Reset multipart assembly when the advertised packet count changes so packets from different messages cannot share storage.

--- xDrip/BluetoothTransmitter/M5Stack/Utilities/M5StackPacket.swift
+++ xDrip/BluetoothTransmitter/M5Stack/Utilities/M5StackPacket.swift
@@ -24,20 +24,22 @@ class M5StackPacket {
     func getText() -> String? {
         
         // check that private properties are initialised
-        guard timeStampLastPacket != nil, let totalNumberOfPacketsToReceive = totalNumberOfPacketsToReceive, packetsReceived.count > 0 else {return nil}
-        
+        guard timeStampLastPacket != nil,
+              let totalNumberOfPacketsToReceive = totalNumberOfPacketsToReceive,
+              packetsReceived.count == totalNumberOfPacketsToReceive else {return nil}
+
         // check if all packets were received
-        for i in 1...totalNumberOfPacketsToReceive {
-            if packetsReceived[i - 1].count == 0 {
+        for packet in packetsReceived {
+            if packet.count == 0 {
                 return nil
             }
         }
         
         // read the string first as data
         var result = Data()
-        for i in 1...totalNumberOfPacketsToReceive {
+        for packet in packetsReceived {
             
-            result.append(packetsReceived[i - 1])
+            result.append(packet)
             
         }
 
@@ -57,29 +59,33 @@ class M5StackPacket {
         // length should be at least 3 bytes, otherwise just ignore
         if value.count < 3 {return}
         
+        let totalNumberOfPackets = Int(value.uint8(position: 2))
+        let numberOfThisPacket = Int(value.uint8(position: 1))
+        guard totalNumberOfPackets > 0,
+              numberOfThisPacket > 0,
+              numberOfThisPacket <= totalNumberOfPackets else {return}
+
         // if more than 200 ms ago since last packet, then reset all properties, looks like a new
         if let timeStampLastPacket = timeStampLastPacket, Date().toMillisecondsAsInt64() - timeStampLastPacket.toMillisecondsAsInt64() > ConstantsM5Stack.maximumTimeBetweenTwoPacketsInMs {
             
             resetAllProperties()
         }
         
+        // a different packet count belongs to a new message, even when it arrives before the timeout
+        if let totalNumberOfPacketsToReceive = totalNumberOfPacketsToReceive, totalNumberOfPacketsToReceive != totalNumberOfPackets {
+            resetAllProperties()
+        }
+
         // set timeStampLastPacket to now
         timeStampLastPacket = Date()
         
-        totalNumberOfPacketsToReceive = Int(value.uint8(position: 2))
-        guard let totalNumberOfPacketsToReceive = totalNumberOfPacketsToReceive else {return}
+        totalNumberOfPacketsToReceive = totalNumberOfPackets
         
         // check packetsReceived, possibly still empty which means this would be the first packet being received
         if packetsReceived.count == 0 {
             // want to have an array of Data, size totalNumberOfPacketsToReceive, actual values will be set as we receive the packets, first one being just a few lines of code later
-            for _ in 1...totalNumberOfPacketsToReceive {
-                packetsReceived.append(Data())
-            }
+            packetsReceived = Array(repeating: Data(), count: totalNumberOfPackets)
         }
-        
-        // get number of this packet
-        let numberOfThisPacket = Int(value.uint8(position: 1))
-        guard numberOfThisPacket <= totalNumberOfPacketsToReceive else {return}// would be a coding error
         
         // assign data
         packetsReceived[numberOfThisPacket - 1] = value.subdata(in: 3..<value.count)
@@ -97,4 +103,3 @@ class M5StackPacket {
     }
     
 }
-
